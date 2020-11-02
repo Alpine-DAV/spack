5ae8704a3bdb9431975108165d7967d700afb761
Cyrus Harrison <cyrush@llnl.gov>
2020-11-02 15:22:16 -0800

    update conduit to use develop (new main branch)

--- var/spack/repos/builtin/packages/conduit/package.py
+++ var/spack/repos/builtin/packages/conduit/package.py
@@ -37,7 +37,7 @@ class Conduit(Package):
     url      = "https://github.com/LLNL/conduit/releases/download/v0.3.0/conduit-v0.3.0-src-with-blt.tar.gz"
     git      = "https://github.com/LLNL/conduit.git"
 
-    version('master', branch='master', submodules=True, preferred=True)
+    version('develop', branch='develop', submodules=True, preferred=True)
     version('0.5.1', sha256='68a3696d1ec6d3a4402b44a464d723e6529ec41016f9b44c053676affe516d44')
     version('0.5.0', sha256='7efac668763d02bd0a2c0c1b134d9f5ee27e99008183905bb0512e5502b8b4fe')
     version('0.4.0', sha256='c228e6f0ce5a9c0ffb98e0b3d886f2758ace1a4b40d00f3f118542c0747c1f52')
